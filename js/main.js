1d44c185b6b8a09de3ff2e04d2e7cab795e91213
John Bueno <bueno@facultycreative.com>
2013-09-30 10:52:58 -0400

    Made dynamic navigation and fall back for ie8

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ $('document').ready(function(){
 	$('.menu li.menu-item').hover(
 		function(){
 			
-			if(!$(this).hasClass('current_page_item') && $(this).find('.sub-menu').length == 0){
+			if(!$(this).hasClass('current_page_item') && $(this).find('.sub-menu').length == 0 && !$(this).parent().hasClass('sub-menu')){
 				$(this).stop(true, false).animate({'height': '30px'}, 150);
 			}
 
 		},
 		function(){
 
-			if(!$(this).hasClass('current_page_item') && $(this).find('.sub-menu').length == 0){
+			if(!$(this).hasClass('current_page_item') && $(this).find('.sub-menu').length == 0 && !$(this).parent().hasClass('sub-menu')){
 				$(this).stop(true, false).animate({'height': '26px'}, 150);
 			}
 
